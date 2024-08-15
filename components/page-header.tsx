68091176bf81ab40f89cb3784fab53a66a3352be
unknown <mihail.diweew@yandex.ru>
2024-08-15 11:05:50 +0300

    clerk auth setup and create header

---
+++ components/page-header.tsx
@@ -0,0 +1,34 @@
+import {
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton
+} from '@clerk/nextjs'
+import Image from 'next/image'
+import React from 'react'
+import { Button } from './ui/button'
+
+const PageHeader = () => {
+    return (
+        <header className='flex h-20 items-center justify-between'>
+            <Image 
+                src={'/default-monochrome.svg'}
+                width={100}
+                height={100}
+                alt='logo'
+            />
+            <div className="">
+                <SignedOut>
+                    <SignInButton>
+                        <Button variant={'outline'} className='text-white'>Sign in</Button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
+        </header>
+    )
+}
+
+export default PageHeader
