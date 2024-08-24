6e0deca767b154855aa8ebeff6ed16e2434e0f22
unknown <mihail.diweew@yandex.ru>
2024-08-24 11:12:59 +0300

    supabase setup with drizzle

--- app/(user)/dashboard/page.tsx
+++ app/(user)/dashboard/page.tsx
@@ -1,7 +1,12 @@
 import NewProjectBtn from '@/components/new-project'
+import { db } from '@/db'
+import { projects } from '@/db/schema'
 import React from 'react'
 
-export default function Page() {
+export default async function Page() {
+    const allProjects = await db.select().from(projects)
+    console.log(allProjects)
+    
     return (
         <NewProjectBtn />
     )
---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { config } from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+
+config({ path: '.env.local' });
+export default defineConfig({
+    schema: "./db/schema.ts",
+    dialect: 'postgresql',
+    migrations: {
+        prefix: 'supabase'
+    },
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    }
+})
